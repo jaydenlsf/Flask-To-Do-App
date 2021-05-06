81132098701837b5f20faeef185ce380019fe87f
Jayden <jaydenlsf47@gmail.com>
2021-05-06 14:20:31 +0000

    Reworked update task endpoint

--- application/routes.py
+++ application/routes.py
@@ -1,51 +1,67 @@
 from application import app, db
 from application.models import Tasks
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
 from application.forms import TaskForm, UpdateForm
 
 @app.route('/')
 def home():
-    return render_template('index.html', title='Homepage')
+    all_tasks = Tasks.query.all()
+    completed_list = []
+    incompleted_list = []
+
+    for task in all_tasks:
+        if task.status == 'incompleted':
+            if task.description == '':
+                task.description = 'No Description'
+                incompleted_list.append(str(task.date_created)[:11] + ' --- ' + task.title.title() + ' --- ' + task.description)
+        else:
+            if task.description == '':
+                task.description = 'No Description'
+                completed_list.append(str(task.date_created)[:11] + ' --- ' + task.title.title() + ' --- ' + task.description)
+
+    if len(completed_list) == 0:
+        completed_list.append('No completed tasks.')
+
+    if len(incompleted_list) == 0:
+        incompleted_list.append('No incompleted tasks.')
+
+    return render_template('index.html', title='Homepage', incompleted_list=incompleted_list, completed_list=completed_list)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     form = TaskForm()
-    if form.validate_on_submit():
-        flash(f'Added new task "{form.task.data}".')
-        new_task = Tasks(title=form.task.data, description=form.description.data, status=form.status.data)
-        print(form.status.data)
-        db.session.add(new_task)
-        db.session.commit()
-        return redirect('/')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_task = Tasks(title=form.task.data, description=form.description.data, status=form.status.data)
+            db.session.add(new_task)
+            db.session.commit()
+            flash(f'Added new task "{form.task.data}".')
+            return redirect(url_for('home'))
     return render_template('add_task.html', title='Add Task', form=form)
 
-@app.route('/all')
-def read():
+@app.route('/update', methods=['GET', 'POST'])
+def update_task():
+    form = UpdateForm()
     all_tasks = Tasks.query.all()
-    task_list = []
-    for task in all_tasks:
-        if task.description == '':
-            task.description = 'No Description'
-        task_list.append(task.title.title() + ' --- ' + task.description + ' --- ' + task.status.title())
-    return render_template('tasks.html', title='View tasks', task_list=task_list)
 
-@app.route('/update/<title>:<update>:<value>')
-def update_task(title, update, value):
-    update_task = Tasks.query.filter_by(title=title).first()
-    if update == 'description':
-        update_task.description = value
-        db.session.commit()
-    elif update == 'status':
-        if value.lower() == 'true':
-            update_task.status = True
-        elif value.lower() == 'false':
-            update_task.status = False
-        else:
-            return 'Unable to update status.'
-        db.session.commit()
-    else:
-        return 'Invalid entry.'
-    return f'Modified "{update}" of task "{title.title()}" to "{value}".'
+    for task in all_tasks:
+        form.task.choices.append((task.id, f'{task.title}'))
+
+    if request.method == 'POST':
+        selected_task_id = form.task.data
+        update_task = Tasks.query.filter_by(id=selected_task).first()
+        if form.validate_on_submit():
+            if form.description.data != '':
+                update_task.description = form.description.data
+            
+            if form.status.data != update_task.status:
+                update_task.status = form.status.data
+
+            db.session.commit()
+            return redirect(url_for('home'))
+            flash(f'Updated task "{form.task.data}"')
+
+    return render_template('update_task.html', title='Update Task', form=form, all_tasks=all_tasks)
 
 @app.route('/delete/<title>')
 def delete(title):
