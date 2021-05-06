bb70e860d5310a4f773427789210daf26cf0c613
Jayden <jaydenlsf47@gmail.com>
2021-05-06 23:18:53 +0000

    Added timezone

--- application/models.py
+++ application/models.py
@@ -1,9 +1,10 @@
 from application import db
-from datetime import date
+from datetime import datetime
+import pytz
 
 
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(30), nullable=False, unique=True)
     status = db.Column(db.String(15), default="incompleted")
-    date_created = db.Column(db.Date, default=date.today())
+    date_created = db.Column(db.Date, default=datetime.now(pytz.timezone("Europe/London")).date())
