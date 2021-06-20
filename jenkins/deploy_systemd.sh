7e5b2563d0ff5fe1a35f9d10a0ae9e7ca40bbeb6
Ubuntu <jaydenlsf@vm.brfjozof3ofurb0xq4aiw5d2yd.zx.internal.cloudapp.net>
2021-06-20 13:50:02 +0000

    Initial commit

---
+++ jenkins/deploy_systemd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install application to /opt/
+rm -r /opt/todo-list
+mkdir /opt/todo-list
+cp -r . /opt/todo-list
+
+# Generate service file
+cat << EOF > todo-list.service
+[Unit]
+Description=Todo List
+
+[Service]
+User=jenkins
+Environment=DATABASE_URI=$DATABASE_URI
+Environment=SECRET_KEY=$SECRET_KEY
+ExecStart=/bin/bash /opt/todo-list/jenkins/startup.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Move service file to systemd
+sudo cp todo-list.service /etc/systemd/system/todo-list.service
+
+# system reload/start/stop
+sudo systemctl daemon-reload
+sudo systemctl stop todo-list
+sudo systemctl start todo-list
---
+++ k8s/secrets/delete_secrets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Remember to set environment variables before running this script
+# DATABASE_URI
+# SECRET_KEY
+# MYSQL_ROOT_PASSWORD
+# MYSQL_DATABASE
+
+if [ -z $DATABASE_URI] || [ -z $SECRET_KEY ] || [ -z $MYSQL_DATABASE ] || [ -z $MYSQL_ROOT_PASSWORD ]
+then
+    echo "One or more of youre secrests has not been set"
+    exit 1
+fi
+
+sad \
+    -e 's,{{DATABASE_URI}},'$DATABASE_URI',g;' \
+    -e 's,{{SECRET_KEY}},'$SECRET_KEY',g;' \
+    -e 's,{{MYSQL_ROOT_PASSWORD}},'$MYSQL_ROOT_PASSWORD',g;' \
+    -e 's,{{MYSQL_DATABASE}},'MYSQL_DATABASE',g;' secrets.yaml | kubectl delete -f -
